858d2f357bf6d62f7e65b6c24adcf6024a247ed7
Timothy <timodb031@gmail.com>
2025-05-04 02:09:06 +0200

    add about_us, contact_us, pricing, and services pages, along with urls and views

--- src/simscare/home/urls.py
+++ src/simscare/home/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from home.views import HomeView
+from home.views import HomeView, AboutUsView, ServicesView, PricingView, ContactUsView
 
 
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
+    path("about_us/", AboutUsView.as_view(), name="about_us"),
+    path("services/", ServicesView.as_view(), name="services"),
+    path("pricing/", PricingView.as_view(), name="pricing"),
+    path("contact_us/", ContactUsView.as_view(), name="contact_us"),
 ]
--- src/simscare/home/views.py
+++ src/simscare/home/views.py
@@ -1,8 +1,23 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 from django.views.generic import TemplateView
 
 # Create your views here.
 class HomeView(TemplateView):
     template_name = "home/home.html"
 
+
+class AboutUsView(TemplateView):
+    template_name = "home/about_us_page.html"
+
+
+class ServicesView(TemplateView):
+    template_name = "home/services_page.html"
+
+
+class PricingView(TemplateView):
+    template_name = "home/pricing_page.html"
+
+
+class ContactUsView(TemplateView):
+    template_name = "home/contact_us_page.html"
 
